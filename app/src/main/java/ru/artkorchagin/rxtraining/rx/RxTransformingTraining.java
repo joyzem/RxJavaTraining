f31bbab476810b918a0cd6f76d6ed444ccbd2462
Rodion Gazizov <rodion.gazizov@bk.ru>
2023-07-07 19:11:22 +0300

    finish RxTransforming

--- app/src/main/java/ru/artkorchagin/rxtraining/rx/RxTransformingTraining.java
+++ app/src/main/java/ru/artkorchagin/rxtraining/rx/RxTransformingTraining.java
@@ -24,7 +24,7 @@ public class RxTransformingTraining {
      * преобразованные из чисел в {@code intObservable}
      */
     public Observable<String> transformIntToString(Observable<Integer> intObservable) {
-        throw new NotImplementedException();
+        return intObservable.map(Object::toString);
     }
 
     /**
@@ -36,7 +36,7 @@ public class RxTransformingTraining {
      * {@code idObservable}
      */
     public Observable<Entity> requestEntityById(Observable<Integer> idObservable) {
-        throw new NotImplementedException();
+        return idObservable.concatMap(this::requestApiEntity);
     }
 
     /**
@@ -48,19 +48,19 @@ public class RxTransformingTraining {
      * поток имён объединённых первой буквой в имени
      */
     public Observable<GroupedObservable<Character, String>> distributeNamesByFirstLetter(Observable<String> namesObservable) {
-        throw new NotImplementedException();
+        return namesObservable.groupBy(name -> name.charAt(0));
     }
 
     /**
      * Объединить элементы, полученные из {@code intObservable} в списки {@link List} с максимальным
      * размером {@code listsSize}
      *
-     * @param listsSize      максимальный размер списка элементов
-     * @param intObservable  {@link Observable} с произвольным количеством рандомных чисел
+     * @param listsSize     максимальный размер списка элементов
+     * @param intObservable {@link Observable} с произвольным количеством рандомных чисел
      * @return {@code Observable} который эммитит списки чисел из {@code intObservable}
      */
     public Observable<List<Integer>> collectsIntsToLists(int listsSize, Observable<Integer> intObservable) {
-        throw new NotImplementedException();
+        return intObservable.buffer(listsSize);
     }
 
     /* Вспомогательные методы */
